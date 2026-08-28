95f8acf58050a7c87f1ea3b9f8b7e39298317d31
Deniskaaaz <www.petuk-denis@yandex.ru>
2026-08-28 22:11:17 +0300

    Update compileSdk and targetSdk to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.tripbot.trip_bot_app"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.tripbot.trip_bot_app"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
