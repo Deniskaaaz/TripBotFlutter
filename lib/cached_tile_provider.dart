49cbbc44d8e7404bb4a86f4acd2a3918eddc0817
Deniskaaaz <www.petuk-denis@yandex.ru>
2026-09-02 11:15:41 +0300

    Update cached_tile_provider.dart

--- lib/cached_tile_provider.dart
+++ lib/cached_tile_provider.dart
@@ -1,12 +1,8 @@
-﻿import 'dart:ui' show DecoderCallback, Codec, instantiateImageCodec;
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CachedTileProvider extends TileProvider {
-  final DefaultCacheManager _cacheManager = DefaultCacheManager();
-
   @override
   ImageProvider getImage(TileCoordinates coordinates, TileLayer options) {
     final url = options.urlTemplate!
@@ -15,32 +11,6 @@ class CachedTileProvider extends TileProvider {
         .replaceFirst('{z}', coordinates.z.toString())
         .replaceFirst('{s}', options.subdomains[coordinates.x % options.subdomains.length]);
 
-    return _CachedImageProvider(url, _cacheManager);
-  }
-}
-
-class _CachedImageProvider extends ImageProvider<_CachedImageProvider> {
-  final String url;
-  final DefaultCacheManager cacheManager;
-
-  _CachedImageProvider(this.url, this.cacheManager);
-
-  @override
-  Future<_CachedImageProvider> obtainKey(ImageConfiguration configuration) {
-    return SynchronousFuture<_CachedImageProvider>(this);
-  }
-
-  @override
-  ImageStreamCompleter load(_CachedImageProvider key, DecoderCallback decode) {
-    return MultiFrameImageStreamCompleter(
-      codec: _loadAsync(key),
-      scale: 1.0,
-    );
-  }
-
-  Future<Codec> _loadAsync(_CachedImageProvider key) async {
-    final file = await cacheManager.getSingleFile(url);
-    final bytes = await file.readAsBytes();
-    return await instantiateImageCodec(bytes);
+    return CachedNetworkImageProvider(url);
   }
 }
